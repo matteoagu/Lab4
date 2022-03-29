36d5f4b270a739dcf8037831eea377fafb9aec46
Matteo Aguiari <aguiarimatteo@gmail.com>
2022-03-29 10:21:56 +0200

    Use vector of 2D points in Regression

--- main.cpp
+++ main.cpp
@@ -1,39 +1,36 @@
-#include "Regression.hpp"
-
 #include <iostream>
 
-int main ()
-{
-    Regression reg;
-    
-    std::cout<<"Questo programma fa il fit a una retta un insieme di numeri inseriti dall'utente.\nDigitare a per aggiungere un punto, digitare f per fare il fit dei punti inseriti.\n\n";
+#include "Regression.hpp"
 
-    while(1<2)
-    {
-        char scelta{};
-        std::cin>>scelta;
+int main() {
+  Regression reg;
 
-        if (scelta=='a')
-        {
-            double x{};
-            double y{};
+  std::cout << "Questo programma fa il fit a una retta un insieme di numeri "
+               "inseriti dall'utente.\nDigitare a per aggiungere un punto, "
+               "digitare f per fare il fit dei punti inseriti.\n\n";
 
-            std::cin>>x>>y;
-            reg.add(x,y);
-        }
+  while (1 < 2) {
+    char scelta{};
+    std::cin >> scelta;
 
-        if (scelta=='f')
-        {
-            Result res = fit(reg);
+    if (scelta == 'a') {
+      double x{};
+      double y{};
 
-            std::cout<<"y = "<<res.A<<" "<<res.B<<"x\n\n";    //fare meglio l'estetica dell'output
-            break;
-        }
-
-        if (scelta!='a' && scelta!='f')
-        {
-            std::cout<<"Entrata non valida!\n";
-        }
+      std::cin >> x >> y;
+      reg.add(x, y);
     }
 
+    if (scelta == 'f') {
+      Result res = fit(reg);
+
+      std::cout << "y = " << res.A << " " << res.B
+                << "x\n\n";  // fare meglio l'estetica dell'output
+      break;
+    }
+
+    if (scelta != 'a' && scelta != 'f') {
+      std::cout << "Entrata non valida!\n";
+    }
+  }
 }
